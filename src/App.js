cd441326cea6a42b5f2a3835472249a847aae090
Sonwabiso Buso <sonwabisob@gmail.com>
2023-11-10 12:12:53 +0200

    Added descriptive comments to the parent files of the app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import { Routes, Route } from "react-router-dom"; // Import React Router components
-import Navigation from "./components/Navigation"; // Import the Navigation component
-import Home from "./components/Home"; // Import the Home component
-import About from "./components/About"; // Import the About component
-import Products from "./components/Products"; // Import the Products component
+import { Routes, Route } from "react-router-dom"; // Importing React Router components
+import Navigation from "./components/Navigation"; // Importing the Navigation component
+import Home from "./components/Home"; // Importing the Home component
+import About from "./components/About"; // Importing the About component
+import Products from "./components/Products"; // Importing the Products component
 import Login from "./components/Login";
 import Cart from "./components/Cart";
-import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS styles
+import "bootstrap/dist/css/bootstrap.min.css"; // Importing Bootstrap CSS styles
 import "./App.css";
-import Registration from "./components/form-validation/Registration"; // Import the Registration component
+import Registration from "./components/form-validation/Registration"; // Importing the Registration component
 
 import React, { useState } from "react";
 
 function App() {
+  // State to manage visibility of total price and the total price itself
   const [isTotalPriceVisible, setTotalPriceVisible] = useState(false);
   const [totalPrice, setTotalPrice] = useState(0);
 
   return (
     <div className="app">
-      {/* Pass totalPrice and setTotalPriceVisible to Navigation */}
+      {/* Navigation component with totalPrice and setTotalPriceVisible props */}
       <Navigation
         totalPrice={totalPrice}
         setTotalPriceVisible={isTotalPriceVisible}
       />
 
+      {/* React Router configuration for different routes */}
       <Routes>
         <Route
           path="/products"
@@ -33,10 +35,15 @@ function App() {
             />
           }
         />
+        {/* Route for the About component */}
         <Route path="/about" element={<About />} />
+        {/* Route for the Login component */}
         <Route path="/login" element={<Login />} />
+        {/* Route for the Cart component */}
         <Route path="/cart" element={<Cart />} />
+        {/* Route for the Registration component */}
         <Route path="/registration" element={<Registration />} />
+        {/* Default route for the Home component */}
         <Route path="/" element={<Home />} />
       </Routes>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,16 +4,20 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./components/store/AuthContext";
+import { AuthProvider } from "./components/store/AuthContext"; // importing the AuthProvider component
 import { Provider } from "react-redux";
-import store from "./components/store/store";
+import store from "./components/store/store"; // importing the store component
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    {/* BrowserRouter for handling client-side navigation */}
     <BrowserRouter>
+      {/* Provider for the Redux store to manage global state */}
       <Provider store={store}>
+        {/* AuthProvider for managing authentication state */}
         <AuthProvider>
+          {/* Main App component */}
           <App />
         </AuthProvider>
       </Provider>
