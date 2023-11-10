cee483f0b6e17689d2f6c330dceb137e2fd3a4b2
Sonwabiso Buso <sonwabisob@gmail.com>
2023-11-10 11:09:05 +0200

    Added descriptive comments to the cartSlice.js store component

--- src/components/store/cartSlice.js
+++ src/components/store/cartSlice.js
@@ -1,26 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+// To define a slice for managing the cart state
 const cartSlice = createSlice({
-  name: "cart",
+  name: "cart", // Slice name
   initialState: {
     items: [], // Array to store cart items
     totalPrice: 0, // Total price of items in the cart
     itemCount: 0, // Number of items in the cart
   },
   reducers: {
+    // Reducer function to handle adding an item to the cart
     addToCart: (state, action) => {
-      state.items = [...state.items, action.payload];
-      state.totalPrice += action.payload.price;
-      state.itemCount += 1;
+      state.items = [...state.items, action.payload]; // To add the new item to the cart
+      state.totalPrice += action.payload.price; // To update the total price
+      state.itemCount += 1; // To increment the item count
     },
+    // Reducer function to handle removing an item from the cart
     removeFromCart: (state, action) => {
+      // To find the item to be removed
       const removedItem = state.items.find(
         (item) => item.id === action.payload.id
       );
       if (removedItem) {
+        // To remove the item from the cart
         state.items = state.items.filter(
           (item) => item.id !== action.payload.id
         );
+        // To update the total price and item count
         state.totalPrice -= removedItem.price;
         state.itemCount -= 1;
       }
@@ -28,6 +34,6 @@ const cartSlice = createSlice({
   },
 });
 
+// To export action creators and the reducer
 export const { addToCart, removeFromCart } = cartSlice.actions;
-
 export default cartSlice.reducer;
