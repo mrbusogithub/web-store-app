ab57317c53652131cd0c5ed93be0b414d4d8ff0e
Sonwabiso Buso <sonwabisob@gmail.com>
2023-11-10 10:57:56 +0200

    Added descriptive comments to the AuthContext.js store component

--- src/components/store/AuthContext.js
+++ src/components/store/AuthContext.js
@@ -1,49 +1,54 @@
 import React, { createContext, useContext, useReducer } from "react";
 
-// Define the initial state
+// To define the initial state
 const initialState = {
   isLoggedIn: false,
   username: "",
 };
 
-// Create the context
+// To create the context
 const AuthContext = createContext(initialState);
 
-// Define the actions
+// To define the actions
 const actionTypes = {
   LOGIN: "LOGIN",
   LOGOUT: "LOGOUT",
 };
 
-// Create the reducer
+// To create the reducer
 const authReducer = (state, action) => {
   switch (action.type) {
+    // Action to handle user login
     case actionTypes.LOGIN:
       return {
         ...state,
         isLoggedIn: true,
         username: action.payload,
       };
+    // Action to handle user logout
     case actionTypes.LOGOUT:
       return {
         ...state,
         isLoggedIn: false,
         username: "",
       };
+    // Default case for unknown actions
     default:
       return state;
   }
 };
 
-// Create a context provider
+// To create a context provider
 const AuthProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, initialState);
 
   return (
+    // To provide the AuthContext with current state and dispatch function
     <AuthContext.Provider value={{ state, dispatch }}>
       {children}
     </AuthContext.Provider>
   );
 };
 
+// To export AuthContext, AuthProvider, and actionTypes for external use
 export { AuthContext, AuthProvider, actionTypes };
